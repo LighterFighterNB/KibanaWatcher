4b2a94692e97e674f42999d7b99d25cf43f8dc83
Lennart van Laaake <lennartvanlaake@gmail.com>
2017-10-02 16:10:54 +0200

    Added methods to send updated object to the Elastic server and to update interval

--- WatcherTest/src/main/java/RESTapi.java
+++ WatcherTest/src/main/java/RESTapi.java
@@ -1,3 +1,5 @@
+
+import java.io.BufferedReader;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -10,25 +12,25 @@ import org.apache.http.nio.entity.NStringEntity;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
-
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.Map;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
-public class RESTapi
-{
-    public static void main(String[] Args) throws IOException
-    {
+public class RESTapi {
+
+    public static void main(String[] Args) throws IOException {
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY,
                 new UsernamePasswordCredentials("Eric", "password"));
 
         RestClientBuilder builder = RestClient.builder(new HttpHost("bc2e0fb1ddbf540185dc508598e610d7.eu-west-1.aws.found.io", 9243, "https"))
-                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback()
-                {
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
                     @Override
-                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder)
-                    {
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
                         return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
                     }
                 });
@@ -40,58 +42,87 @@ public class RESTapi
 
         String trigger = "";
 
-        String jsonString = "{\n" +
-                "  \"trigger\": {\n" +
-                "    \"schedule\": {\n" +
-                "      \"interval\": \"50s\"\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"input\": {\n" +
-                "    \"search\": {\n" +
-                "      \"request\": {\n" +
-                "        \"search_type\": \"query_then_fetch\",\n" +
-                "        \"indices\": [\n" +
-                "          \"shakes*\"\n" +
-                "        ],\n" +
-                "        \"types\": [],\n" +
-                "        \"body\": {\n" +
-                "          \"size\": 10,\n" +
-                "          \"query\": {\n" +
-                "            \"bool\": {\n" +
-                "              \"must\": {\n" +
-                "                \"match\": {\n" +
-                "                  \"play_name\": \"Henry IV\"\n" +
-                "                }\n" +
-                "              }\n" +
-                "            }\n" +
-                "          }\n" +
-                "        }\n" +
-                "      }\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"condition\": {\n" +
-                "    \"compare\": {\n" +
-                "      \"ctx.payload.hits.total\": {\n" +
-                "        \"gt\": 0\n" +
-                "      }\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"actions\": {\n" +
-                "    \"log\": {\n" +
-                "      \"logging\": {\n" +
-                "        \"level\": \"info\",\n" +
-                "        \"text\": \"WARNING\"\n" +
-                "      }\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
+        String jsonString = "{\n"
+                + "  \"trigger\": {\n"
+                + "    \"schedule\": {\n"
+                + "      \"interval\": \"50s\"\n"
+                + "    }\n"
+                + "  },\n"
+                + "  \"input\": {\n"
+                + "    \"search\": {\n"
+                + "      \"request\": {\n"
+                + "        \"search_type\": \"query_then_fetch\",\n"
+                + "        \"indices\": [\n"
+                + "          \"shakes*\"\n"
+                + "        ],\n"
+                + "        \"types\": [],\n"
+                + "        \"body\": {\n"
+                + "          \"size\": 10,\n"
+                + "          \"query\": {\n"
+                + "            \"bool\": {\n"
+                + "              \"must\": {\n"
+                + "                \"match\": {\n"
+                + "                  \"play_name\": \"Henry IV\"\n"
+                + "                }\n"
+                + "              }\n"
+                + "            }\n"
+                + "          }\n"
+                + "        }\n"
+                + "      }\n"
+                + "    }\n"
+                + "  },\n"
+                + "  \"condition\": {\n"
+                + "    \"compare\": {\n"
+                + "      \"ctx.payload.hits.total\": {\n"
+                + "        \"gt\": 0\n"
+                + "      }\n"
+                + "    }\n"
+                + "  },\n"
+                + "  \"actions\": {\n"
+                + "    \"log\": {\n"
+                + "      \"logging\": {\n"
+                + "        \"level\": \"info\",\n"
+                + "        \"text\": \"WARNING\"\n"
+                + "      }\n"
+                + "    }\n"
+                + "  }\n"
+                + "}";
 
-        HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
-
-        //Response response = restClient.performRequest("PUT", endpointWatcher+"my-watch",params, entity);
-        Response response = restClient.performRequest("PUT", endpointWatcher + "my-watch/_activate");
-
+        Response getRequest = restClient.performRequest("GET", endpointWatcher + "my-watch");
+        // get the data from the watcher
+        JSONObject myobject = new JSONObject(EntityUtils.toString(getRequest.getEntity()));
+        // converts data from watcher to object
+        setInterval(myobject, "2h");
+        // changes the interval of the watcher
+        updateWatcher(myobject, restClient, endpointWatcher+"my-watch", params);
+        // updates the watcher
         restClient.close();
 
+    }
+
+    public static void updateWatcher(JSONObject JSON, RestClient restClient, String endpoint, Map<String, String> params) throws IOException {
+        
+        JSON = JSON.getJSONObject("watch");
+        String trigger = JSON.getJSONObject("trigger").toString();
+        String input = JSON.getJSONObject("input").toString();
+        String condition = JSON.getJSONObject("condition").toString();
+        String actions = JSON.getJSONObject("actions").toString();
+        String jsonString = "{\n"
+                + "  \"trigger\":" + trigger + ",\n"
+                + "  \"input\":" + input + ",\n"
+                + "  \"condition\":" + condition + ",\n"
+                + "  \"actions\":" + actions + "}";
+        System.out.println("The string sent was: " + jsonString);
+        HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
+        Response response = restClient.performRequest("PUT", endpoint, params, entity);
+        System.out.println("The response received was: " + EntityUtils.toString(response.getEntity()));
+
+    }
+    
+    public static void setInterval(JSONObject JSON, String interval) {
+        
+        JSONObject schedule = JSON.getJSONObject("watch").getJSONObject("trigger").getJSONObject("schedule");
+        schedule.put("interval", interval);
+        
     }
 }

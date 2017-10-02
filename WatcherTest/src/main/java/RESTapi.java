da30f928671e15837d0e05600ad0ecf32bcad625
LighterFighterNB <smith.eric.george@gmail.com>
2017-10-02 13:12:52 +0200

    new upload

--- WatcherTest/src/main/java/RESTapi.java
+++ WatcherTest/src/main/java/RESTapi.java
@@ -1,14 +1,19 @@
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.http.nio.entity.NStringEntity;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 
 public class RESTapi
 {
@@ -27,8 +32,62 @@ public class RESTapi
                 });
 
         RestClient restClient = builder.build();
+        String endpointWatcher = "/_xpack/watcher/watch/";
 
-        Response response = restClient.performRequest("PUT", "/_xpack/watcher/watch/my-watch/_activate");
+        Map<String, String> params = Collections.emptyMap();
+
+        String trigger ="";
+
+        String jsonString = "{\n" +
+                "  \"trigger\": {\n" +
+                "    \"schedule\": {\n" +
+                "      \"interval\": \"50s\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"input\": {\n" +
+                "    \"search\": {\n" +
+                "      \"request\": {\n" +
+                "        \"search_type\": \"query_then_fetch\",\n" +
+                "        \"indices\": [\n" +
+                "          \"shakes*\"\n" +
+                "        ],\n" +
+                "        \"types\": [],\n" +
+                "        \"body\": {\n" +
+                "          \"size\": 10,\n" +
+                "          \"query\": {\n" +
+                "            \"bool\": {\n" +
+                "              \"must\": {\n" +
+                "                \"match\": {\n" +
+                "                  \"play_name\": \"Henry IV\"\n" +
+                "                }\n" +
+                "              }\n" +
+                "            }\n" +
+                "          }\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"condition\": {\n" +
+                "    \"compare\": {\n" +
+                "      \"ctx.payload.hits.total\": {\n" +
+                "        \"gt\": 0\n" +
+                "      }\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"actions\": {\n" +
+                "    \"log\": {\n" +
+                "      \"logging\": {\n" +
+                "        \"level\": \"info\",\n" +
+                "        \"text\": \"WARNING\"\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+
+        HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
+
+        //Response response = restClient.performRequest("PUT", endpointWatcher+"my-watch",params, entity);
+        Response response = restClient.performRequest("PUT",endpointWatcher+"my-watch/_deactivate");
 
     }
 }
